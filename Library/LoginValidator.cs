2bd5ba53302a26b0211db5b63ee419faa6010adb
ulshinvv <158988350+ulshinvv@users.noreply.github.com>
2024-04-08 11:58:45 +0700

    Add files via upload

---
+++ Library/LoginValidator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidatorLibrary
+{
+    public class LoginValidator : IValidator
+    {
+        public bool Validate(string validateObject)
+        {
+            if (string.IsNullOrEmpty(validateObject))
+                return false;
+
+            if (validateObject.Length < 6 || validateObject.Length > 16)
+                return false;
+
+            foreach (char c in validateObject)
+            {
+                if (!char.IsLetter(c) || !char.IsLower(c))
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Library/PasswordValidator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidatorLibrary
+{
+    public class PasswordValidator : IValidator
+    {
+        public bool Validate(string validateObject)
+        {
+            if (string.IsNullOrEmpty(validateObject))
+                return false;
+
+            if (validateObject.Length < 8 || validateObject.Length > 20)
+                return false;
+
+            bool hasUpperCase = false;
+            bool hasDigit = false;
+            bool hasSpecialChar = false;
+
+            foreach (char c in validateObject)
+            {
+                if (char.IsUpper(c))
+                    hasUpperCase = true;
+                else if (char.IsDigit(c))
+                    hasDigit = true;
+                else if ("!@#$%^&*-_".Contains(c))
+                    hasSpecialChar = true;
+            }
+
+            return hasUpperCase && hasDigit && hasSpecialChar;
+        }
+    }
+}
